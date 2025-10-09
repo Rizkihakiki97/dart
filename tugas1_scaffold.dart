c605a2e0533666686fc31b9fb1c55ec78f93147a
Rizkihakiki97 <158152048+Rizkihakiki97@users.noreply.github.com>
2025-10-09 10:49:19 +0700

    Add files via upload

---
+++ tugas1_scaffold.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class Tugas1Scaffold extends StatelessWidget {
+  const Tugas1Scaffold({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Profile saya")),
+      backgroundColor: const Color.fromARGB(255, 243, 191, 196),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          spacing: 10,
+          children: [
+            Text(
+              "Nama : Muhamad rizki hakiki",
+              style: TextStyle(
+                fontSize: 20,
+                color: const Color.fromARGB(255, 10, 10, 10),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(Icons.location_on),
+                Container(
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      colors: [Colors.red, Colors.yellow, Colors.green],
+                    ),
+                  ),
+                  child: Text("Jakarta Pusat", style: TextStyle(fontSize: 17)),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text("Lagi belajar di PPKD Jakarta pusat, Programming Mobile."),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
